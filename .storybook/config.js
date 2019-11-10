d34f76504461b5b698011bc220fb148a93a29300
samuraikun <tazitaziawawa@gmail.com>
2019-11-10 15:39:41 +0900

    add font-awesome in Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,18 @@ import { configure, addDecorator } from '@storybook/vue';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from 'storybook-addon-vue-info';
 import '@storybook/addon-console';
+import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faEye,
+  faEyeSlash
+)
+
+Vue.component('v-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
 
 // automatically import all files ending in *.stories.js
 const loadStories = require.context("../src", true, /stories\.js$/);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './components/App'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
