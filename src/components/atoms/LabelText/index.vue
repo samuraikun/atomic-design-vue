c8d59fe626cfd8317ec69a941600e04ae27cd5b6
samuraikun <tazitaziawawa@gmail.com>
2019-11-10 15:40:17 +0900

    add atom LabelText component

---
+++ src/components/atoms/LabelText/index.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+  span.a-label-text {{ text }}
+</template>
+
+<script>
+export default {
+  name: 'LabelText',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+.a-label-text {
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 1.2;
+  color: var(--color-mono-2);
+}
+</style>
